363d057bf703b823c9d099665fa03bbf2963e8bf
njava <wqk@njava.com>
2024-10-15 14:52:20 +0800

    add OneGirlModelFluxPromptsNode 一个女孩模特提示词,按照flux格式输出clip_l和t5xxl提示词

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
+from .py_src.onegirl_model_flux_prompts_note import OneGirlModelFluxPromptsNode
 from .py_src.size_node import SizeNode
 
-NODE_CLASS_MAPPINGS = {SizeNode.NAME: SizeNode}
+NODE_CLASS_MAPPINGS = {
+    SizeNode.NAME: SizeNode,
+    OneGirlModelFluxPromptsNode.NAME: OneGirlModelFluxPromptsNode,
+}
 
-NODE_DISPLAY_NAME_MAPPINGS = {SizeNode.NAME: "图片尺寸|ImageSize"}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    SizeNode.NAME: "图片尺寸|ImageSize",
+    OneGirlModelFluxPromptsNode.NAME: "一个女模特提示词"
+}
 
 print("\033[34mComfyUI 🐻‍❄️Polar's Tools: \033[92mLoaded\033[0m")
 print()
