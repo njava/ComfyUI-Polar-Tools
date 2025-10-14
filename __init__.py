aa6305d022ca1d986cfb31edcd25716199024cd4
njava <wqk@njava.com>
2025-10-14 11:01:53 +0800

    重构节点,1 图片尺寸节点. 2 一个女孩节点. 3 古风美女节点. 4 wan22运镜节点

--- __init__.py
+++ __init__.py
@@ -1,14 +1,20 @@
-from .py_src.onegirl_model_flux_prompts_note import OneGirlModelFluxPromptsNode
-from .py_src.size_node import SizeNode
+from .py_src.node_prompts_onegirl import PromptsOneGirlNode
+from .py_src.node_image_size import ImageSizeNode
+from .py_src.node_prompts_wan22_shooting import Wan22ShootingNode
+from .py_src.node_prompts_gufeng_beauty import GufengBeautyNode
 
 NODE_CLASS_MAPPINGS = {
-    SizeNode.NAME: SizeNode,
-    OneGirlModelFluxPromptsNode.NAME: OneGirlModelFluxPromptsNode,
+    ImageSizeNode.NAME: ImageSizeNode,
+    PromptsOneGirlNode.NAME: PromptsOneGirlNode,
+    Wan22ShootingNode.NAME: Wan22ShootingNode,
+    GufengBeautyNode.NAME: GufengBeautyNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    SizeNode.NAME: "图片尺寸|ImageSize",
-    OneGirlModelFluxPromptsNode.NAME: "一个女模特提示词|OneGirlModel Flux.1"
+    ImageSizeNode.NAME: "图片尺寸|ImageSize",
+    PromptsOneGirlNode.NAME: "一个女孩|OneGirlModel",
+    Wan22ShootingNode.NAME: "Wan2.2视频镜头|Wan2.2",
+    GufengBeautyNode.NAME: "古风美女|GuFengBeauty"
 }
 
 print("\033[34mComfyUI 🐻‍❄️Polar's Tools: \033[92mLoaded\033[0m")
